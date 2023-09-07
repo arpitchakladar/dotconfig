75c2c8dbfdc48bdea34e738932cbd2ea0eae38ae
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2023-09-07 13:29:51 +0530

    Changed package manager of neovim to something more stable.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,6 @@
-require("plugins");
 require("keybindings");
+require("plugins");
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.o.laststatus = 3
 vim.cmd "highlight EndOfBuffer ctermfg=bg"
 vim.o.number = true
@@ -16,4 +14,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		vim.o.expandtab = false
 	end
 })
-
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,37 +1,65 @@
-require("neo-tree").setup {
-	default_component_configs = {
-		indent = {
-			indent_size = 2,
-			padding = 0
-		}
-	},
-	window = {
-		width = 30
-	},
-	filesystem = {
-		filtered_items = {
-			hide_hidden = false,
-			hide_gitignored = false,
-			hide_dotfiles = false
-		}
-	}
-}
-require("nvim-web-devicons").setup()
-require("lualine").setup()
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd "colorscheme gruvbox"
-
-require("packer").startup(function(use)
-	use "wbthomason/packer.nvim"
-	use "morhetz/gruvbox"
-	use "nvim-lualine/lualine.nvim"
-	use {
+require("lazy").setup({
+	{
+		"morhetz/gruvbox",
+		config = function()
+			vim.cmd "colorscheme gruvbox"
+		end
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup()
+		end
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+		config = function()
+			require("nvim-web-devicons").setup()
+		end
+	},
+	{
 		"nvim-neo-tree/neo-tree.nvim",
-		requires = {
+		keys = {
+			{ "<c-n>", "<cmd>Neotree toggle<cr>", desc = "NeoTree" }
+		},
+		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		}
+			"MunifTanjim/nui.nvim"
+		},
+		lazy = false,
+		init = function()
+			require("neo-tree").setup {
+				default_component_configs = {
+					indent = {
+						indent_size = 2,
+						padding = 0
+					}
+				},
+				window = {
+					width = 30
+				},
+				filesystem = {
+					filtered_items = {
+						hide_hidden = false,
+						hide_gitignored = false,
+						hide_dotfiles = false
+					}
+				}
+			}
+		end
 	}
-	use "nvim-tree/nvim-web-devicons"
-end)
+})
